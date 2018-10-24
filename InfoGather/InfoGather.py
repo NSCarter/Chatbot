bbc1af9fe2b346379dcf7d55af5a9208c88fe784
NSCarter <30809794+NSCarter@users.noreply.github.com>
2018-10-24 16:22:50 +0100

    Prints certain columns depending on the column name

--- InfoGather/InfoGather.py
+++ InfoGather/InfoGather.py
@@ -2,6 +2,6 @@ import numpy as np
 
 file = 'General Names.csv'
 
-names = np.loadtxt(file, delimiter=',', dtype=str, skiprows=1)  #Loads the contents of the csv file, seperates it by comma and converts it to string
+name = np.genfromtxt(file, delimiter=',', names=True, dtype=None)   #Loads the contents of the csv file, seperates it by comma and converts it to string
 
-print(names)
+print(name['Drinking'])  
