240525bfbccb10d87c7bd5952f08ab34d853489d
NSCarter <30809794+NSCarter@users.noreply.github.com>
2018-10-24 16:29:34 +0100

    Finds only the top result

--- Individual Place Search.py
+++ Individual Place Search.py
@@ -2,23 +2,21 @@
 import urllib.request
 import json
 
-userInput = 'where+is+the+wheather' #the input from the user
+userInput = 'singer+hall' #the input from the user
 
 #acces the website
-with urllib.request.urlopen('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=52.408040,-1.511378&radius=10000&rankby=prominence&keyword='+userInput+'&rst=1&key=AIzaSyDtTDDYIt-fYijfy9c-mpBkZKOAoNhj1j8') as response:
+with urllib.request.urlopen('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=52.408040,-1.511378&radius=10000&rankby=prominence&keyword='+userInput+'&key=AIzaSyDtTDDYIt-fYijfy9c-mpBkZKOAoNhj1j8') as response:
     raw = response.read()   #read the data from the website
 
 data = json.loads(raw.decode('utf-8'))  #decode the data into a dictionary
 
-#look at each item in the dictionary
-for i in range(len(data['results'])):
-    #find the place ID and and the location
-    placeID = data['results'][i]['place_id']
-    reference = data['results'][i]['reference']
-    geometry = data['results'][i]['geometry']
-    iD = data['results'][i]['id']
-    #print the place ID and the location
-    print('\n'+placeID)
-    print(reference+'\n')
-    print(geometry,'\n')
-    print(iD+'\n')
+#Finds the ID and the location of the first result
+placeID = data['results'][0]['place_id']
+reference = data['results'][0]['reference']
+geometry = data['results'][0]['geometry']
+iD = data['results'][0]['id']
+#print the ID and the location
+print('\n'+placeID)
+print(reference+'\n')
+print(geometry,'\n')
+print(iD+'\n')
