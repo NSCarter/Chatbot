30b7e6e0b8f31b3c1e615d238a66aa9b630d08ce
NSCarter <30809794+NSCarter@users.noreply.github.com>
2018-10-24 16:31:53 +0100

    Put the program into a function

--- Individual Place Search.py
+++ Individual Place Search.py
@@ -2,38 +2,45 @@
 import urllib.request
 import json
 
-userInput = 'alma building' #The input from the user
+userInput = 'singer hall' #The input from the user
 userInput = userInput.replace(' ', '+') #Turn the input into something that can be used in the url
 
-#Access the results from the following web address
-with urllib.request.urlopen('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=52.408040,-1.511378&radius=10000&rankby=prominence&keyword='+userInput+'&key=AIzaSyDtTDDYIt-fYijfy9c-mpBkZKOAoNhj1j8') as response:
-    raw = response.read()   #Read the data
+def individualPlaceSearch(userInput):
+    '''Takes an input from a user as a string, finds information about a place, and returns it as a string'''
+    
+    userInput = userInput.replace(' ', '+') #Turn the input into something that can be used in the url
+    
+    #Access the results from the following web address
+    with urllib.request.urlopen('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=52.408040,-1.511378&radius=10000&rankby=prominence&keyword='+userInput+'&key=AIzaSyDtTDDYIt-fYijfy9c-mpBkZKOAoNhj1j8') as response:
+        raw = response.read()   #Read the data
 
-data = json.loads(raw.decode('utf-8'))  #Decode the data into a dictionary
+    data = json.loads(raw.decode('utf-8'))  #Decode the data into a dictionary
 
-#Finds the ID and the location of the first result
-placeID = data['results'][0]['place_id']
+    #Finds the ID and the location of the first result
+    placeID = data['results'][0]['place_id']
 
-#Access the results from the following web address
-with urllib.request.urlopen('https://maps.googleapis.com/maps/api/place/details/json?placeid='+placeID+'&key=AIzaSyDtTDDYIt-fYijfy9c-mpBkZKOAoNhj1j8') as response:
-    raw2 = response.read()   #Read the data
+    #Access the results from the following web address
+    with urllib.request.urlopen('https://maps.googleapis.com/maps/api/place/details/json?placeid='+placeID+'&key=AIzaSyDtTDDYIt-fYijfy9c-mpBkZKOAoNhj1j8') as response:
+        raw2 = response.read()   #Read the data
 
-data2 = json.loads(raw2.decode('utf-8'))    #Decode the data into a dictionary
+    data2 = json.loads(raw2.decode('utf-8'))    #Decode the data into a dictionary
 
-#The information to be found for the user
-information = ['name','rating','formatted_address','open_now','formatted_phone_number','website']
+    #The information to be found for the user
+    information = ['name','rating','formatted_address','open_now','formatted_phone_number','website']
 
-#Check if the information we want to find is available in the data2 variable and output the information
-for i in information:
-    if i == 'open_now':
-        if 'opening_hours' in data2['result']:
-            if data2['result']['opening_hours'][i] == True:
-                    print('Open now')
-            else:
-                print('Closed now')
-    else:
-        if i in data2['result']:
-            if i == 'rating':
-                print(data2['result'][i], 'stars')
-            else:
-                print(data2['result'][i])
+    #Check if the information we want to find is available in the data2 variable and output the information
+    for i in information:
+        if i == 'open_now':
+            if 'opening_hours' in data2['result']:
+                if data2['result']['opening_hours'][i] == True:
+                        print('Open now')
+                else:
+                    print('Closed now')
+        else:
+            if i in data2['result']:
+                if i == 'rating':
+                    print(data2['result'][i], 'stars')
+                else:
+                    print(data2['result'][i])
+
+individualPlaceSearch(userInput)
